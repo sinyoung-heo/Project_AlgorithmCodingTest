f288b9ecaa2d0c069112947f72210ec5fa7431d2
HSY <hsykpu@gmail.com>
2021-09-28 10:54:33 +0900

    봉우리. 2차원 배열 탐색. 현재 위치를 기준으로 상, 하, 좌, 우 값을 탐색 및 비교한다. 현재 위치가 봉우리이면 상하좌우 인덱스는 절대로 봉우리가 될 수 없으므로 CheckComplete 컨테이너를 통해 flag를 설정해준다. CheckComplete가 true이면 값 비교를 건너뛰도록 한다.

---
+++ SampleProject/SampleProject/Problem47.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+	47. 봉우리
+
+	지도 정보가 N*N 격자판에 주어집니다. 각 격자에는 그 지역의 높이가 쓰여있습니다. 
+	각 격자판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 
+	봉우리 지역이 몇 개 있는 지 알아내는 프로그램을 작성하세요.
+	격자의 가장자리는 0으로 초기화 되었다고 가정한다.
+	만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
+	0 0 0 0 0 0 0
+	0 5 3 7 2 3 0	-> 3
+	0 3 7 1 6 1 0	-> 2
+	0 7 2 5 3 4 0	-> 2
+	0 4 3 6 4 1 0	-> 1
+	0 8 7 3 5 2 0	-> 2
+	0 0 0 0 0 0 0
+
+	▣ 입력설명
+	첫 줄에 자연수 N이 주어진다.(1<=N<=50)
+	두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 
+	각 자연수는 100을 넘지 않는다.
+
+	▣ 출력설명
+	봉우리의 개수를 출력하세요.
+
+	▣ 입력예제 1
+	5
+	5 3 7 2 3
+	3 7 1 6 1
+	7 2 5 3 4
+	4 3 6 4 1
+	8 7 3 5 2
+
+	▣ 출력예제 1
+	10
+*/
+
+int answer = 0;
+int n = 0;
+vector<vector<bool>> vecCheckComplete;
+vector<vector<int>> vecHeight;
+
+int main()
+{
+	cin >> n;
+
+	int input = 0;
+
+	for (int y = 0; y < n; ++y)
+	{
+		vecHeight.emplace_back();
+		vecCheckComplete.emplace_back();
+
+		for (int x = 0; x < n; ++x)
+		{
+			cin >> input;
+			vecHeight[y].emplace_back(input);
+			vecCheckComplete[y].emplace_back(false);
+		}
+	}
+
+	for (int y = 0; y < vecHeight.size(); ++y)
+	{
+		for (int x = 0; x < vecHeight[y].size(); ++x)
+		{
+			if (vecCheckComplete[y][x])
+				continue;
+
+			int curHeight = vecHeight[y][x];
+
+			// 상
+			if (y - 1 > -1)
+			{
+				if (curHeight <= vecHeight[y - 1][x])
+					continue;
+			}
+			// 하
+			if (y + 1 < vecHeight.size())
+			{
+				if (curHeight <= vecHeight[y + 1][x])
+					continue;
+			}
+			// 좌
+			if (x - 1 > -1)
+			{
+				if (curHeight <= vecHeight[y][x - 1])
+					continue;
+			}
+			// 우
+			if (x + 1 < vecHeight[y].size())
+			{
+				if (curHeight <= vecHeight[y][x + 1])
+					continue;
+			}
+
+			// 현재 위치가 봉우리이면? 상하좌우는 비교할 필요 X.
+			++answer;
+			vecCheckComplete[y][x] = true;
+
+			if (y - 1 > -1)
+				vecCheckComplete[y - 1][x] = true;	// 상
+			if (y + 1 < vecHeight.size())
+				vecCheckComplete[y + 1][x] = true;	// 하
+			if (x - 1 > -1)
+				vecCheckComplete[y][x - 1] = true;	// 좌
+			if (x + 1 < vecHeight[y].size())
+				vecCheckComplete[y][x + 1] = true;	// 우
+		}
+	}
+
+	cout << answer << endl;
+}
